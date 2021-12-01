a42284532f049cdb0dc6913cb79ff0244b9f9273
Nick Gleadall <ng384@cam.ac.uk>
2021-12-01 09:45:41 +0000

    Init

---
+++ 1/part_1/day_1.py
@@ -0,0 +1,14 @@
+
+import sys
+
+f = open( sys.argv[1])
+inputs = [ int(i) for i in f.readlines()]
+
+c = 0
+
+for i in range( 1 , len(inputs)):
+
+    if inputs[ i - 1 ] < inputs[i]:
+        c += 1
+
+print( "Number of increases: {}".format( c ))
